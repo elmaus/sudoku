4b2596981e27d4f3b19b2f20d1a760adc5c59870
Samuel Camus <42428075+elmaus@users.noreply.github.com>
2021-10-30 04:01:39 +0300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -18,7 +18,7 @@ const level = document.getElementById('level');
 // const choiceColorBox = document.querySelector(".choice-color-box");
 
 let mainbw = mainBox.clientWidth; 
-mainBox.style.width = mainbw - (3 * 9); 
+mainBox.style.width = mainbw - (5 * 9); 
 
 
 let toggleColor = false;
