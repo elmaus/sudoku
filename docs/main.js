a3b23024241ac52b88077b6175aa586499b09303
Samuel Camus <42428075+elmaus@users.noreply.github.com>
2021-10-30 20:21:09 +0300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -333,7 +333,7 @@ for(let i=0; i<9; i++){
                     selectedX = i;
                     selectedY = j;
                 }
-                //addNodes(i, j);
+                addNodes(i, j);
             }
         });
 
